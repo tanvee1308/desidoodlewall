374c1a3786d5a9247467de6eaa3f0510cfaab5e9
tanvee1308 <tanvee1308@gmail.com>
2025-10-24 11:59:36 +0100

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+     'use client';
 import { useEffect, useState } from 'react';
 import DrawingCanvas from '../components/DrawingCanvas';
 import { makeShareImage } from '../lib/makeShareImage';
